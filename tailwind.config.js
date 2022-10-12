72eb132e48fa4690e3779df8def17b217c1dd42c
Tojonirina Fetraniaina Rarivoarison <arison@MacBook-Pro-de-Tojonirina.local>
2022-10-12 19:31:31 +0300

    Define theme color and extends the theme screen in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.jsx",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+    ],
+    theme: {
+        screens: {
+            xs: "460px",
+            ...defaultTheme.screens,
+        },
+        extend: {
+            colors: {
+                primary: "#334156",
+                secondary: "#4F5470",
+                accent: "#FB923C",
+                "accent-secondary": "#f97316",
+            },
+        },
+    },
+    plugins: [],
+};
